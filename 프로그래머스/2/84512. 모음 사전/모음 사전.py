8c1ea86521717ccd41d009c694d7c9d3a6f1ce19
hyunzsu <108773199+hyunzsu@users.noreply.github.com>
2024-03-19 15:28:20 +0900

    [level 2] Title: 모음 사전, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.py
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.py
@@ -1,13 +1,5 @@
 def solution(word):
-	preset = {
-		'A': [1, 1, 1, 1, 1],
-		'E': [782, 157, 32, 7, 2],
-		'I': [1563, 313, 63, 13, 3], 
-		'O': [2344, 469, 94, 19, 4],
-		'U': [3125, 625, 125, 25, 5]
-	}
-    
 	res = 0
-	for i, key in enumerate(word):
-		res += preset[key][i]
+	for i, n in enumerate(word):
+		res += (5 ** (5 - i) - 1) / (5 - 1) * 'AEIOU'.index(n) + 1
 	return res
