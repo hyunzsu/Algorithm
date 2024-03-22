9c78a97ddeabaa2e418353c92d8d17a2e1b35fdc
hyunzsu <108773199+hyunzsu@users.noreply.github.com>
2024-03-22 14:52:19 +0900

    [level 2] Title: 기능개발, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.py
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -1,15 +1,10 @@
 def solution(progresses, speeds):
-    answer = []
-    while progresses:
-        for i in range(len(progresses)):
-            progresses[i] += speeds[i]
-        
-        cnt = 0
-        while progresses and progresses[0] >= 100:
-            progresses.pop(0)
-            speeds.pop(0)
-            cnt += 1
-        if cnt > 0:
-            answer.append(cnt)
-    
-    return answer
+	answer = []
+	for progresses, speed in zip(progresses, speeds):
+		left = -((progresses - 100) // speed)
+		if not answer or answer[-1][0] < left:
+			answer.append([left, 1])
+		else:
+			answer[-1][1] += 1
+			
+	return [a[1] for a in answer]
