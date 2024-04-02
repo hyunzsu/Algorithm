743de5a3b0f4d6a69c068996d3b29b3542bce1f3
hyunzsu <108773199+hyunzsu@users.noreply.github.com>
2024-04-02 23:21:05 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.06 ms, Memory: 9.94 MB -BaekjoonHub

--- 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.py
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.py
@@ -1,11 +1,11 @@
+def collatz(num, cnt):
+    if num == 1: return cnt
+    if cnt == 500: return -1
+
+    if num % 2 == 0:
+        return collatz(num // 2, cnt + 1)
+    elif num % 2 == 1:
+        return collatz(num * 3 + 1, cnt + 1)
+
 def solution(num):
-    cnt = 0
-    while num != 1:
-        if num % 2 == 0:
-            num /= 2
-        else:
-            num = num * 3 + 1
-        cnt += 1
-        if cnt == 500:
-            return -1
-    return cnt
+    return collatz(num, 0)
