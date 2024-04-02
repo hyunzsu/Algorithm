59beaeb5e7ecc593a4e2fcb25a71fbe45b74ad5f
hyunzsu <108773199+hyunzsu@users.noreply.github.com>
2024-04-03 01:29:53 +0900

    [level 1] Title: 신규 아이디 추천, Time: 0.20 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.py
+++ 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.py
@@ -1,20 +1,8 @@
 def solution(new_id):
-    res = new_id.lower()
+    answer = new_id.lower()
     filtered = []
-    for c in res:
+    for c in answer:
         if c.isalpha() or c.isdigit() or c in ('-', '_', '.'):
             filtered.append(c)
-    res = ''.join(filtered)
-    while '..' in res:
-        res = res.replace('..', '.')
-    res = res.strip('.')
-    if res == '':
-        res = 'a'
-    if len(res) > 15:
-        res = res[:15]
-    if res[-1] == '.':
-        res = res[:-1]
-    while len(res) < 3:
-        res += res[-1]
-    return res
-        
+    answer = ''.join(filtered)
+    print(answer)
