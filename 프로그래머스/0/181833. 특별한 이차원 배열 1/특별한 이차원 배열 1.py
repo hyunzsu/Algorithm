45e169b89ff66eecb9d4d082ac70b3db1abef7bc
hyunzsu <108773199+hyunzsu@users.noreply.github.com>
2024-04-06 09:01:27 +0900

    [level 0] Title: 특별한 이차원 배열 1, Time: 0.42 ms, Memory: 10.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181833. 특별한 이차원 배열 1/특별한 이차원 배열 1.py
@@ -0,0 +1,11 @@
+def solution(n):
+    res = []
+    for i in range(n):
+        row = []
+        for j in range(n):
+            if i == j:
+                row.append(1)
+            else:
+                row.append(0)
+        res.append(row)
+    return res
