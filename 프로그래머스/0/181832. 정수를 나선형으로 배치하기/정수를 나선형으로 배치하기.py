ab379467e1983ba032521dd4c5e87299520594b1
hyunzsu <108773199+hyunzsu@users.noreply.github.com>
2024-04-09 01:09:37 +0900

    [level 0] Title: 정수를 나선형으로 배치하기, Time: 0.75 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/181832. 정수를 나선형으로 배치하기/정수를 나선형으로 배치하기.py
+++ 프로그래머스/0/181832. 정수를 나선형으로 배치하기/정수를 나선형으로 배치하기.py
@@ -1,42 +1,18 @@
 def solution(n):
-    # 2차원 배열 초기화
+    dy = [0, 1, 0, -1]
+    dx = [1, 0, -1, 0]
+    y, x = 0, -1
+
     res = [[0] * n for _ in range(n)]
-    
-    # 초기 위치 및 값 설정
-    value = 1
-    row, col = 0, 0
+    cnt = 1
     direction = 0
-    
-    # 시계 방향 나선형으로 값 배치
-    while value <= n * n:
-        res[row][col] = value
-        
-        # 다음 위치 계산
-        if direction == 0:  # 오른쪽
-            col += 1
-            if col == n or res[row][col] != 0:
-                col -= 1
-                row += 1
-                direction = 1
-        elif direction == 1:  # 아래쪽
-            row += 1
-            if row == n or res[row][col] != 0:
-                row -= 1
-                col -= 1
-                direction = 2
-        elif direction == 2:  # 왼쪽
-            col -= 1
-            if col < 0 or res[row][col] != 0:
-                col += 1
-                row -= 1
-                direction = 3
-        else:  # 위쪽
-            row -= 1
-            if row < 0 or res[row][col] != 0:
-                row += 1
-                col += 1
-                direction = 0
-        
-        value += 1
-    
+    while cnt <= n**2:
+        ny, nx = y + dy[direction], x + dx[direction]
+        if 0 <= ny < n and 0 <= nx < n and not res[ny][nx]:
+            res[ny][nx] = cnt
+            cnt += 1
+            y, x = ny, nx
+        else:
+            direction = (direction + 1) % 4
+
     return res
