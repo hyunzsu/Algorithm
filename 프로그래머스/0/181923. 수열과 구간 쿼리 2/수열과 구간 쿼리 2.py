8b301ebfdac7217631e61ab53e360bfe86eae95b
hyunzsu <108773199+hyunzsu@users.noreply.github.com>
2024-05-01 14:45:45 +0900

    [level 0] Title: 수열과 구간 쿼리 2, Time: 7.34 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.py
+++ 프로그래머스/0/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.py
@@ -2,8 +2,8 @@ def solution(arr, queries):
     res = []
     for s, e, k in queries:
         tmp = []
-        for i in arr[s:e+1]:
-            if i > k:
-                tmp.append(i)
+        for i in range(s, e + 1):
+            if arr[i] > k:
+                tmp.append(arr[i])
         res.append(min(tmp) if tmp else -1)
     return res
