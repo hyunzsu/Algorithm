5bc651da3c48131e3c4647c08eede827e1a15ab1
hyunzsu <108773199+hyunzsu@users.noreply.github.com>
2024-05-03 22:28:21 +0900

    [level 1] Title: 햄버거 만들기, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.py
@@ -0,0 +1,11 @@
+def solution(ingredient):
+    stack = []
+    hamburger = 0   
+    for i in ingredient:
+        stack.append(i)
+        if len(stack) >= 4:
+            if stack[-4:] == [1, 2, 3, 1]:
+                for _ in range(4):
+                    stack.pop()
+                hamburger += 1
+    return hamburger
