6699cb188cd7e20b82cc3a3de8e1f5bfbe64d18d
hyunzsu <108773199+hyunzsu@users.noreply.github.com>
2024-05-06 23:26:18 +0900

    [level 1] Title: 콜라 문제, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.py
@@ -0,0 +1,6 @@
+def solution(a, b, n):
+    res = 0
+    while n >= a: # 가지고 있는 콜라병이 교환 가능한 동안에
+        res += (n // a * b) # 빈 병 개수 추가
+        n = (n % a) + (n // a * b) # 교환하지 못하고 남은 병 + 새로 얻은 콜라
+    return res
