aa4f8935ae40ba4f52342e5b090378da76ec2e19
hyunzsu <108773199+hyunzsu@users.noreply.github.com>
2024-05-13 05:27:47 +0900

    [level 0] Title: 문자열 여러 번 뒤집기, Time: 1.82 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/181913. 문자열 여러 번 뒤집기/문자열 여러 번 뒤집기.py
+++ 프로그래머스/0/181913. 문자열 여러 번 뒤집기/문자열 여러 번 뒤집기.py
@@ -1,5 +1,5 @@
 def solution(my_string, queries):
-    for query in queries:
-        s, e = query
-        my_string = my_string[:s] + my_string[s:e+1][::-1] + my_string[e+1:]
-    return my_string
+    my_string = list(my_string)  
+    for s, e in queries:
+        my_string[s:e+1] = my_string[s:e+1][::-1]  
+    return ''.join(my_string) 
