5a0daca33eb30d3e850dea7f8335a5e9efa9058e
hyunzsu <108773199+hyunzsu@users.noreply.github.com>
2024-05-14 18:43:51 +0900

    [level 0] Title: 부분 문자열 이어 붙여 문자열 만들기, Time: 0.04 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/181911. 부분 문자열 이어 붙여 문자열 만들기/부분 문자열 이어 붙여 문자열 만들기.py
+++ 프로그래머스/0/181911. 부분 문자열 이어 붙여 문자열 만들기/부분 문자열 이어 붙여 문자열 만들기.py
@@ -1,5 +1,2 @@
 def solution(my_strings, parts):
-    res = []
-    for i, p in zip(my_strings, parts):
-        res.append(i[p[0]:p[1]+1])
-    return ''.join(res)
+    return ''.join(i[p[0]:p[1]+1] for i, p in zip(my_strings, parts))
