8b93da96cda44d509fd62c451c439d4b0c19d8ef
hyunzsu <108773199+hyunzsu@users.noreply.github.com>
2024-05-14 18:53:52 +0900

    [level 0] Title: 접미사 배열, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/181909. 접미사 배열/접미사 배열.py
+++ 프로그래머스/0/181909. 접미사 배열/접미사 배열.py
@@ -1,4 +1,2 @@
 def solution(my_string):
-    suffixes = [my_string[i:] for i in range(len(my_string))]
-    sort = sorted(suffixes)
-    return sort
+     return sorted([my_string[i:] for i in range(len(my_string))])
