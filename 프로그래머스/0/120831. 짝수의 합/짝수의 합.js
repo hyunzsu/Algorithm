ac94d9447cc96d995991a44a2afcfa6d532ac60a
hyunzsu <108773199+hyunzsu@users.noreply.github.com>
2025-05-13 22:17:58 +0900

    [level 0] Title: 짝수의 합, Time: 0.10 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120831. 짝수의 합/짝수의 합.js
+++ 프로그래머스/0/120831. 짝수의 합/짝수의 합.js
@@ -1,7 +1,7 @@
 function solution(n) {
-    let result = 0;
-    for(i=0;i<=n;i+=2){
-        result += i
+    var answer = 0;
+    for(let i = 0; i <= n; i += 2){
+        answer += i;
     }
-    return result
+    return answer;
 }
